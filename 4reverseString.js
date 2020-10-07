e5cac12748d71eb73fd432a1bc18506f669c15d4
Magomed Gasanov <magomed-gasanov1996@yandex.ru>
2020-10-07 22:16:25 +0300

    finish 4reverseString

---
+++ 4reverseString.js
@@ -0,0 +1,7 @@
+// Function
+function reverse(str) {
+    return str.split(" ").map(element => element.split('').reverse().join('')).join(" ");
+}
+
+// Testing
+console.log(reverse(" A fun little challenge! "));
