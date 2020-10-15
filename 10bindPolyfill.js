470deadc34a260eef3d8721633a9da2f41168722
Magomed Gasanov <magomed-gasanov1996@yandex.ru>
2020-10-15 12:30:00 +0300

    finish bindPolyfill

--- 10bindPolyfill.js
+++ 10bindPolyfill.js
@@ -2,19 +2,25 @@
 
 
 // // Function
-// Function.prototype.myBind = function (context) {
-//     const obj = this;
-// 	return function(...args) {
-// 		return obj.apply(
-// 			context,
-// 			args
-// 		);
-// 	}
-// }
+Function.prototype.myBind = function (context, ...rest) {
+    let MainFunction = this;
+    return function(...args) {
+        return MainFunction.apply(context, rest.concat(args))
+    }
+}
 
-// // Testing
-// function addPropToNumber(number, arg1) { return this.prop + number*arg1; }
-// let bound = addPropToNumber.myBind({ prop: 9 },3);
-// console.log(bound(1)); // 10
 
-Задача еще в работе. Пытаюсь разобраться, пока еще не понял до конца.
+
+// Testing
+const person = {
+    name: "Magomed"
+}
+
+function sayHiTo(city,country) {
+    console.log(`Hello: ${this.name} from ${city}, ${country}`);
+}
+
+sayHiTo.myBind(person,"Saint-Petersburg")("Russia");
+
+
+
